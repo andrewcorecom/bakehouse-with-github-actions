2861e8c737a63f32d6f8aa248421d468ef05a0fc
andrewpmilne <andrew_p_milne@yahoo.co.uk>
2026-06-11 15:25:54 +0100

    correct yaml

--- bakehouse-app/client/src/pages/UserProfile.jsx
+++
@@ -1,9 +0,0 @@
-export default function UserProfile() {
-    return (
-      <div>
-        <h1>This is your personal profile</h1>
-        <br></br>
-        <p>In order to be seeing this you must be logged in </p>
-      </div>
-    );
-  }
